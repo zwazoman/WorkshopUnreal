7a1084b1e4f7dd710a0aab76b8a546cbef875f5b
nathan-exe <nathan.tazi.1@gmail.com>
2025-03-05 17:47:34 +0100

    pitié

--- WorshopVR2025/Plugins/MetaXR/Source/OculusXRScene/Private/OculusXRSceneDelegates.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Meta Platforms, Inc. and affiliates.
-
-#include "OculusXRSceneDelegates.h"
-
-FOculusXRSceneEventDelegates::FOculusXRBoundaryVisibilityChanged FOculusXRSceneEventDelegates::OculusBoundaryVisibilityChanged;
-
-FOculusXRSceneEventDelegates::FOculusXRSceneCaptureCompleteDelegate FOculusXRSceneEventDelegates::OculusSceneCaptureComplete;
