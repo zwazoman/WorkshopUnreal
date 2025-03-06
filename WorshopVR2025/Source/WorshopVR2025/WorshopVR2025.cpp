e754e073cb6c31f3becb79b4b54abdc6a0a9c200
nathan-exe <nathan.tazi.1@gmail.com>
2025-03-06 16:21:28 +0100

    pitié g peur cpp

---
+++ WorshopVR2025/Source/WorshopVR2025/WorshopVR2025.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WorshopVR2025.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WorshopVR2025, "WorshopVR2025" );
