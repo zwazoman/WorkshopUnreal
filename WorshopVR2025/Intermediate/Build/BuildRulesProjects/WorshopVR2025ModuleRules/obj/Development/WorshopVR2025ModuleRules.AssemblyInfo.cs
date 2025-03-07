9a21b9697291bfce433cd0892c6465dc7b1e4fa2
elagmae <lea.pouthier@gmail.com>
2025-03-07 11:04:08 +0100

    si ça casse c'est la faute de Nestor

--- WorshopVR2025/Intermediate/Build/BuildRulesProjects/WorshopVR2025ModuleRules/obj/Development/WorshopVR2025ModuleRules.AssemblyInfo.cs
+++ WorshopVR2025/Intermediate/Build/BuildRulesProjects/WorshopVR2025ModuleRules/obj/Development/WorshopVR2025ModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorshopVR2025ModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ad1153fefe7028e7f34810c9dd3508633f98dea")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorshopVR2025ModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorshopVR2025ModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
